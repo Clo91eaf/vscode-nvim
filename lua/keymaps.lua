edddc573fced444bf7ff91e45d28f56d46727e45
Clo91eaf <Clo91eaf@qq.com>
2025-05-28 09:32:53 +0800

    remove all keymaps to use vscode keymap
    Signed-off-by: Clo91eaf <Clo91eaf@qq.com>

--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -1,8 +1,1 @@
 vim.keymap.set('v', 'p', '"_dP', { desc = 'Paste without yanking replaced text' })
-
-vim.keymap.set({ "n", "x", "i" }, "<C-d>", function()
-  vscode.with_insert(function()
-    vscode.action("editor.action.addSelectionToNextFindMatch")
-  end)
-end)
-
